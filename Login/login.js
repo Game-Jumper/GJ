3e995d73c9783489b5ee0edf7f728db8c85618bf
LukeStro47 <lukestrong47@gmail.com>
2017-09-29 19:32:22 -0400

    Login

---
+++ Login/login.js
@@ -0,0 +1,17 @@
+/*function check(form) {
+   if(form.userid.value == "jkarch" && form.pwd.value == "Fzc6Q") {
+       window.open("tester.html");
+   } else if(form.userid.value == "jgoldman" && form.pwd.value == "Gz6fWK") {
+       window.open("tester.html");
+   } else if(form.userid.value == "nharrison" && form.pwd.value == "SAm82r") {
+       window.open("tester.html");
+   } else if(form.userid.value == "lstrong" && form.pwd.value == "tock8avg") {
+       window.open("tester.html");
+   } else if(form.userid.value == "pdugan" && form.pwd.value == "L47VdW") {
+       window.open("tester.html");     
+   } else if(form.userid.value == "cheinold" && form.pwd.value == "N5fQfC") {
+       window.open("tester.html");
+   } else {
+       alert("Invalid Login Credentials");
+   }
+}*/
