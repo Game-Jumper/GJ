0bd782852133c69cc7f6e8b2ef702427aff853c5
LukeStro47 <lukestrong47@gmail.com>
2018-10-28 12:17:15 -0400

    New!

--- game.js
+++ game.js
@@ -8,25 +8,33 @@ var config = {
 };
 firebase.initializeApp(config);
 initApp();
-//var sEmail = Cookies.get('email');
-//var sPassword = Cookies.get('password');
-
-
-/*firebase.auth().signInWithEmailAndPassword(sEmail, sPassword).then(function() {
-    document.getElementById('login').innerHTML = "Logged In " + "(" + firebase.auth().currentUser.email + ")";
-    document.getElementById('recent').innerHTML = "Recently Played";
-    document.getElementById('recent').style.cursor = "pointer";
-}).catch(function(error) {
-    return true;
-});*/
 var cEmail;
-var cPassword
-//
+var cPassword;
+const unsave = false;
+function checkUser(gameId) {
+    if(firebase.auth().currentUser != null) {
+        firebase.database().ref('users/' + firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+            var data = snapshot.val();
+            if(data.saves != null) {
+                for(var i = 1; i < Object.Keys(data.saves).length + 1; i++) {
+                    if(data.saves[i] == gameId) {
+                        unsave = true;
+                        document.getElementById('save').innerHTML = "Unsave Game";
+                        break;
+                    }
+                }
+            }
+        });
+    }
+}
 function miniGamesClick() {
-    document.getElementById('asteroids').style.display = "block";
+    document.getElementById('noSave').style.display = "none";
+    if(firebase.auth().currentUser != null) {
+        document.getElementById('asteroids').style.display = "block";
+        document.getElementById('newAge').style.display = "block";
+    }
     document.getElementById('snake').style.display = "block";
     document.getElementById('pong').style.display = "block";
-    document.getElementById('newAge').style.display = "block";
     document.getElementById('helicopter').style.display = "none";
     document.getElementById('form').style.display = "none";
     document.getElementById('congrats').style.display = "none";
@@ -35,6 +43,7 @@ function miniGamesClick() {
     document.getElementById('change').style.display = "none";
 }
 function fullGamesClick() {
+    document.getElementById('noSave').style.display = "none";
     document.getElementById('helicopter').style.display = "block";
     document.getElementById('asteroids').style.display = "none";
     document.getElementById('snake').style.display = "none";
@@ -47,11 +56,14 @@ function fullGamesClick() {
     document.getElementById('change').style.display = "none";
 }
 function allGamesClick() {
+    document.getElementById('noSave').style.display = "none";
+    if(firebase.auth().currentUser != null) {
+        document.getElementById('asteroids').style.display = "block";
+        document.getElementById('newAge').style.display = "block";
+    }
     document.getElementById('helicopter').style.display = "block";
-    document.getElementById('asteroids').style.display = "block";
     document.getElementById('snake').style.display = "block";
     document.getElementById('pong').style.display = "block";
-    document.getElementById('newAge').style.display = "block";
     document.getElementById('form').style.display = "none";
     document.getElementById('congrats').style.display = "none";
     document.getElementById('forgot').style.display = "none";
@@ -59,7 +71,7 @@ function allGamesClick() {
     document.getElementById('change').style.display = "none";
 }
 function logInClick() {
-    initApp();
+    document.getElementById('noSave').style.display = "none";
     document.getElementById('helicopter').style.display = "none";
     document.getElementById('asteroids').style.display = "none";
     document.getElementById('snake').style.display = "none";
@@ -67,68 +79,35 @@ function logInClick() {
     document.getElementById('newAge').style.display = "none";
     document.getElementById('form').style.display = "block";
     document.getElementById('forgot').style.display = "block";
-    if(firebase.auth().currentUser.uid.length > 5) {
-        console.log(firebase.auth().currentUser);
+    if(firebase.auth().currentUser != null) {
+        document.getElementById('change').style.display = "block";
         document.getElementById('congrats').style.display = "block";
         document.getElementById('form').style.display = "none";
-        document.getElementById('login').innerHTML = "Logged In " + "(" + firebase.auth().currentUser.email + ")";
+        document.getElementById('real').style.display = "none";
+        if(firebase.auth().currentUser.emailVerified == false) {
+            document.getElementById('verify').style.display = "block";
+        }
     }
-    function signMeIn() {
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-            //Cookies.set('email', email);
-            //Cookies.set('password', password);
-            //cEmail = Cookies.get('email');
-            document.getElementById('login').innerHTML = "Logged In " + "(" + firebase.auth().currentUser.email + ")";
-            document.getElementById('congrats').style.display = "block";
-            document.getElementById('form').style.display = "none";
-            document.getElementById('real').style.display = "none";
-            document.getElementById('recent').innerHTML = "Recently Played";
-            document.getElementById('recent').style.cursor = "pointer";
-        }).catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode === 'auth/wrong-password') {
-                alert('Invalid Login Credentials');
-            } else {
-                alert(errorMessage + ' Try Again in a Few Seconds');
-            }
-        });
-    }
-    function initApp() {
-        // Listening for auth state changes.
-        // [START authstatelistener]
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                // User is signed in.
-                var displayName = user.displayName;
-                var email = user.email;
-                var emailVerified = user.emailVerified;
-                var uid = user.uid;
-                var providerData = user.providerData;
-                console.log("In");
-                // [START_EXCLUDE]
-                /*document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-                document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-                document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');*/
-                document.getElementById('congrats').style.display = "block";
-                document.getElementById('form').style.display = "none";
-                document.getElementById('real').style.display = "none";
-                document.getElementById('change').style.display = "block";
-                if(firebase.auth().currentUser.emailVerified == false) {
-                    document.getElementById('verify').style.display = "block";
-                }
-                document.getElementById('login').innerHTML = "Logged In " + "(" + firebase.auth().currentUser.email + ")";
-                document.getElementById('recent').innerHTML = "Recently Played";
-                document.getElementById('recent').style.cursor = "pointer";
-            } else {
-                console.log("No User");
-                document.getElementById('login').innerHTML = "Login";
-            }
-        });
-        document.getElementById('sign-in').addEventListener('click', signMeIn, false);
-    }
+}
+function signMeIn() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+        document.getElementById('change').style.display = "block";
+        document.getElementById('congrats').style.display = "block";
+        if(firebase.auth().currentUser.emailVerified == false) {
+            document.getElementById('verify').style.display = "block";
+        }
+        initApp();
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+            alert('Invalid Login Credentials');
+        } else {
+            alert(errorMessage + ' Try Again in a Few Seconds');
+        }
+    });
 }
 function recentGamesClick() {
     if(firebase.auth().currentUser.w == "game-jumper-customer.firebaseapp.com") {
@@ -141,37 +120,20 @@ function recentGamesClick() {
         document.getElementById('forgot').style.display = "none";
         document.getElementById('verify').style.display = "none";
         document.getElementById('change').style.display = "none";
-        if(Cookies.get('asteroids') == "true") {
-            document.getElementById('asteroids').style.display = "block";
-        }
-        if(Cookies.get('snake') == "true") {
-            document.getElementById('snake').style.display = "block";
-        }
-        if(Cookies.get('pong') == "true") {
-            document.getElementById('pong').style.display = "block";
-        }
-        if(Cookies.get('newAge') == "true") {
-            document.getElementById('newAge').style.display = "block";
-        }
-        if(Cookies.get('helicopter') == "true") {
-            document.getElementById('helicopter').style.display = "block";
-        }
+        document.getElementById('noSave').style.display = "none";
+        firebase.database().ref('users/' + firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+            var data = snaphot.val();
+            if(data.saves != null) {
+                for(var i = 0; i < Object.keys(data.saves).length + 1; i++) {
+                    if(data.saves[i] != null) {
+                        document.getElementById(data.saves[i]).style.display = "block";
+                    }
+                }
+            } else {
+                document.getElementById('noSave').style.display = "block";
+            }
+        });
     }
-}
-function asteroidsClick() {
-    Cookies.set('asteroids', 'true');
-}
-function snakeClick() {
-    Cookies.set('snake', 'true');
-}
-function pongClick() {
-    Cookies.set('pong', 'true');
-}
-function newAgeClick() {
-    Cookies.set('newAge', 'true');
-}
-function helicopterClick() {
-    Cookies.set('helicopter', 'true');
 }
 function checkFeature() {
     if(firebase.auth().currentUser != null) {
@@ -183,5 +145,73 @@ function checkFeature() {
 function verifyAccount() {
     firebase.auth().currentUser.sendEmailVerification().then(function() {
         alert("Sent!");
+    });
+}
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            if(document.getElementById('game-main') == null) {
+                document.getElementById('login').innerHTML = "Logged In " + "(" + firebase.auth().currentUser.email + ")";
+                document.getElementById('form').style.display = "none";
+                document.getElementById('real').style.display = "none";
+                document.getElementById('recent').innerHTML = "Saved Games";
+                document.getElementById('recent').style.cursor = "pointer";
+            }
+        } else {
+            console.log("No User");
+            if(document.getElementById('auth') != null) {
+                location.replace('../../pricing.html');
+            }
+        }
+    });
+}
+function saveGame(gameId) {
+    if(firebase.auth().currentUser != null) {
+        firebase.database().ref('users/' + firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+            var data = snapshot.val();
+            if(data.saves != null) {
+                if(unsave == true) {
+                    for(var i = 1; i < Object.keys(data.saves).length + 1; i++) {
+                        if(data.saves[i] == gameId) {
+                            firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/saves/' + i).remove();
+                            unsave = false;
+                            document.getElementById('save').innerHTML = "Save Game";
+                            finishRemove(i);
+                            break;
+                        }
+                    }
+                } else {
+                    for(var e = 1; e < Object.keys(data.saves).length + 1; e++){
+                        if(data.saves[i] == null) {
+                            firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/saves').update({
+                                [e]: gameId
+                            });
+                        }
+                    }
+                    unsave = true;
+                    document.getElementById('save').innerHTML = "Unsave Game";
+                }
+            } else {
+                firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/saves').update({
+                    1: gameId
+                }):
+                unsave = true;
+                document.getElementById('save').innerHTML = "Unsave Game";
+            }
+        });
+    } else {
+        alert("This is a premium feature.");
+    }
+}
+function finishRemove(startNum) {
+    firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/saves').once('value').then(function(snapshot) {
+        var data = snapshot.val();
+        for(var i = startNum + 1; i < Object.keys(data.saves).length + 2; i++) {
+            if(data.saves[i] != null) {
+                firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/saves').update({
+                    [parseInt(i) - 1]: data.saves[i]
+                });
+            }
+        }
     });
 }
