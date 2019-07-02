859ce02b3ce983fc6e7f8a044e0c1b6ac1300fe1
LukeStro47 <lukestrong47@gmail.com>
2019-07-02 17:38:28 -0400

    Signed-off-by: LukeStro47 <lukestrong47@gmail.com>

--- Login/testerlogin.js
+++ Login/testerlogin.js
@@ -1,6 +1,3 @@
-/*// Initialize Firebase
-(function(){*/
-//ADD HEY, (X); SIGNOUT BUTTON
 var config = {
     apiKey: "AIzaSyBsnqMW3L_2pjboOOzZJ8fJDYzGgCxcyoo",
     authDomain: "game-jumper-tester.firebaseapp.com",
@@ -10,280 +7,15 @@ var config = {
     messagingSenderId: "457793690679"
 };
 firebase.initializeApp(config);
-    /*//Get Elements
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    //Add Login Event
-    btnLogin.addEventListener('click', e => {
-        //Get Email & Pass
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        //Sign In
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
-    });
-    //Add RealTime Listener
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if(firebaseUser) {
-            console.log(firebaseUser);
-        } else {
-            console.log('Not Logged In');
-        }
-    });
-}());*/
 function toggleSignIn() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    if (email.length < 4) {
-        alert('Invalid Login Credentials');
-        return;
-    }   
-    if (password.length < 4) {
-        alert('Invalid Login Credentials');
-        return;
-    }
-    // Sign in with email and pass.
-    // [START authwithemail]
     firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-        //Luke
-        if(firebase.auth().currentUser.uid == "Znk2Zr5kmUWQ4SldxGuD7AD8ACg1") {
-            console.log("Luke - Test Account");
-            var usersName = "Test";
-            var userData = {
-                currentMoney: "TEST VERSION",
-                nextPayday: "TEST VERSION",
-                lukeMessages: "TEST VERSION",
-                estimatedMoney: "TEST VERSION"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Jake
-        } else if(firebase.auth().currentUser.uid == "ag5fcDkeIAQO4wpeDBHHYBwo2ju1") {
-            console.log("Jake");
-            var usersName = "Jake";
-            var userData = {
-                currentMoney: "$0.70",
-                nextPayday: "March 22",
-                lukeMessages: "You will get paid $0.50 overall for this game.",
-                estimatedMoney: "$3.75"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Jason
-        } else if(firebase.auth().currentUser.uid == "7v5GdmYyAka4bzWO2KWJ9Ou8R472") {
-            console.log("Jason");
-            var usersName = "Jason";
-            var userData = {
-                currentMoney: "$0.05",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Game Jumper
-        } else if(firebase.auth().currentUser.uid == "sfRa7FV7RkaXg2Nq8rpCM6HB3sF3") {
-            console.log("Test Account");
-            var usersName = "Test";
-            var userData = {
-                currentMoney: "TEST VERSION",
-                nextPayday: "TEST VERSION",
-                lukeMessages: "TEST VERSION",
-                estimatedMoney: "TEST VERSION"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Eamon
-        } else if(firebase.auth().currentUser.uid == "T2yzm2nCcVYMFjcNnkFn48WFTrw1") {
-            console.log("Eamon");
-            var usersName = "Eamon";
-            var userData = {
-                currentMoney: "$0.15",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Luke V
-        } else if(firebase.auth().currentUser.uid == "Q4r3VTu0UNX2fW4qufKksTz2tli2") {
-            console.log("Luke II");
-            var usersName = "Luke";
-            var userData = {
-                currentMoney: "$0.15",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Paul
-        } else if(firebase.auth().currentUser.uid == "E8VLEEeJxmhZ5guXDlCeb98ZHtc2") {
-            console.log("Paul");
-            var usersName = "Paul";
-            var userData = {
-                currentMoney: "$0.20",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Nathan
-        } else if(firebase.auth().currentUser.uid == "IGiaOqJpPfXjiyJSSNmmcFBWt333") {
-            console.log("Nathan");
-            var usersName = "Nathan";
-            var userData = {
-                currentMoney: "$0.15",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-            //Ryan
-        } else if(firebase.auth().currentUser.uid == "q9MLq3OyqBhhp0qKWCi60fiBIWj1") {
-            console.log("Ryan");
-            var usersName = "Ryan";
-            var userData = {
-                currentMoney: "$0.00",
-                nextPayday: "March 22",
-                lukeMessages: "Contact Luke if you are interested in Linguine Coin payments.",
-                estimatedMoney: "$1.00"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-        } else if(firebase.auth().currentUser.uid == "KNeZHx2TXETcGkWLXeYrugQuUHm1") {
-            console.log("Jason O");
-            var usersName = "Jason";
-            var userData = {
-                currentMoney: "$0.00",
-                nextPayday: "March 22",
-                lukeMessages: "Payday Soon!",
-                estimatedMoney: "$0.50"
-            };
-            document.getElementById('email').value = "";
-            document.getElementById('password').value = "";
-            var userOverallData = document.getElementById('testerSection');
-            userOverallData.style.display = "block";
-            var loginData = document.getElementById('login-page');
-            loginData.style.display = "none";
-            var helloText = document.getElementById('hello-text');
-            helloText.style.display = "block";
-            document.getElementById('yourMoney').innerHTML = userData.currentMoney;
-            document.getElementById('estimation').innerHTML = userData.estimatedMoney;
-            document.getElementById('payDay').innerHTML = userData.nextPayday;
-            document.getElementById('messagesFro').innerHTML = userData.lukeMessages;
-            document.getElementById('hello-text').innerHTML = "Hello, " + usersName + "!";
-        } else {
-            console.log("Not signed in");
-        }
+        setUser();
+        getJobs();
     }).catch(function(error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // [START_EXCLUDE]
         if (errorCode === 'auth/wrong-password') {
           alert('Invalid Login Credentials');
         } else {
@@ -291,38 +23,71 @@ function toggleSignIn() {
         }
         console.log(error);
         document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
     });
 }
-
-/*initApp handles setting up UI event listeners and registering Firebase auth listeners:
-- firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-out, and that is where we update the UI.*/
-
 function initApp() {
-    // Listening for auth state changes.
-    // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // [START_EXCLUDE]
-            /*document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');*/
+            setUser();
+            getJobs();
         } else {
             var helloText = document.getElementById('hello-text');
-            /*userOverview.style.display = "none";*/
             helloText.style.display = "none";
         }
     });
-    // [END authstatelistener]
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+function setUser() {
+    firebase.database().ref('Users/' + firebase.auth().currentUser.uid).once('value').then(function(snapshot) {
+        var data = snapshot.val();
+        document.getElementById('testerSection').style.display = "block";
+        document.getElementById('login-page').style.display = "none";
+        document.getElementById('hello-text').style.display = "block";
+        document.getElementById('yourMoney').innerHTML = data.tester.currentMoney;
+        document.getElementById('payDay').innerHTML = data.tester.nextPayday;
+        document.getElementById('messagesFro').innerHTML = data.tester.messagesFrom;
+        var split = data.name.split(' ');
+        var fname = split[0].split('');
+        document.getElementById('hello-text').innerHTML = fname[0] + ". " + split[1];
+    });
 }
 window.onload = function() {
     initApp();
 };
+function getJobs() {
+    firebase.database().ref('Jobs').once('value').then(function(snapshot) {
+        var data = snapshot.val();
+        var amt = Object.keys(data).length - 1;
+        if(data["0"] == null) {
+            var text = document.createElement("p");
+            text.classList.add("center");
+            text.innerHTML = "No Jobs Currently Available";
+            document.getElementById("jobs").appendChild(text);
+        } else {
+            for(var i = 0; i < amt; i++) {
+                var name = data[i].name;
+                var link = data[i].link;
+                if(data[i].done == true) {
+                    var text = document.createElement("p");
+                    text.classList.add("center");
+                    var inside = document.createElement("a");
+                    inside.classList.add("done");
+                    inside.innerHTML = name;
+                    inside.href = link;
+                    inside.disabled = true;
+                    text.appendChild(inside);
+                    document.getElementById("jobs").appendChild(text);
+                } else {
+                    var text = document.createElement("p");
+                    text.classList.add("center");
+                    var inside = document.createElement("a");
+                    inside.classList.add("blue");
+                    inside.innerHTML = name;
+                    inside.href = link;
+                    text.appendChild(inside);
+                    document.getElementById("jobs").appendChild(text);
+                }
+            }
+        }
+    });
+}
